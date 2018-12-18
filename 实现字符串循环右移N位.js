9e477fa9e461e4e47f8ef5b5386ea63eb809dae9
yangyi <yangaiday@gmail.com>
2018-12-18 10:13:23 +0800

    Add files via upload

---
+++ 实现字符串循环右移N位.js
@@ -0,0 +1,17 @@
+//方法1，数组法
+function rightchange(char,n){
+ 	//以"空"转为数组
+ 	var arr = char.split("");
+ 	//循环执行右移
+ 	for(var i = 1 ; i <= n ; i++){
+ 		arr.unshift(arr.pop());
+ 	}
+ 	return arr.join("");
+ }
+
+//方法2
+ function rightchange(char,n){
+			n = n % char.length;	//子串长度，移动n位，等于移动1位
+			if(n == 0) return char;	//移动0位，就是返回原字符串	
+			return char.substr(-n) + char.substr(0,char.length - n);
+		}
